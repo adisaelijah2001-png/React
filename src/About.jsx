4da30de36267cf51e462c37f76aeaf28ce7b5d36
Adisa Elijah Abisola <adisaelijah2001@gmail.com>
2026-08-18 12:26:03 +0100

    Blogs.filter

--- src/About.jsx
+++ src/About.jsx
@@ -5,11 +5,12 @@ const About = () => {
     const [blogs, setBlogs] = useState([
         {title: 'My new website', body: 'Body like tea', author: 'mario', id: '1'},
         {title: 'welcome to party', body: 'Body like tea', author: 'yoshi', id: '2'},
-        {title: 'web developer top tips', body: 'normally', author: 'Elijah', id: '3'}
+        {title: 'web developer top tips', body: 'normally', author: 'mario', id: '3'}
     ]);
     return (
         <div className='About'>
            <BlogList blogs={blogs} title="All blogs" />
+           <BlogList blogs={blogs.filter((blog) => blogs.authour === 'mario')} title="mario blogs" />
         </div>
     );
 }
